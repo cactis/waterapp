368fb199afb3150c403293cba7217f464d8fc87d
譚吉翰 <jimmy@I.local>
2017-06-30 14:39:54 +0800

    category added

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ git_source(:github) do |repo_name|
 end
 
 
+gem 'awesome_nested_set'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails'#, '~> 5.0.2'
 # Use mysql as the database for Active Record
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170518060029) do
+ActiveRecord::Schema.define(version: 20170630062515) do
+
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name"
+    t.integer "parent_id"
+    t.integer "lft", null: false
+    t.integer "rgt", null: false
+    t.integer "depth", default: 0, null: false
+    t.integer "children_count", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["lft"], name: "index_categories_on_lft"
+    t.index ["parent_id"], name: "index_categories_on_parent_id"
+    t.index ["rgt"], name: "index_categories_on_rgt"
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
